6002704e15b9330a876278b3c4653fcea3fbb6c7
Sanjeev Santhosh <sanjeevsanthosh143@gmail.com>
2025-08-03 17:08:55 +0530

    day 6 completed VOICE RECOGNITION COMPLETED IN BACKEND

--- engine/command.py
+++ engine/command.py
@@ -1,13 +1,39 @@
 import pyttsx3
 
+import speech_recognition as sr
+
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices') 
     engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 164)
+    engine.setProperty('rate', 154)
     print(voices)
     engine.say(text)
     engine.runAndWait()
 
-speak("India is my country, all indians are my brothers and sisters")
+def takeCommand():
+
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text = takeCommand()
+
+speak(text)
