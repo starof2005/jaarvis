1ea4c38fb09440b413be58feb6c85e811d9934f0
Sanjeev Santhosh <sanjeevsanthosh143@gmail.com>
2025-08-03 15:17:36 +0530

    added mic, msg settings

--- www/main.js
+++ www/main.js
@@ -1,13 +1,39 @@
 $(document).ready(function () {
 
-    $('.text').textillate({
+$('.text').textillate({
+loop: true,
+sync: true,
+in: {
+effect: "bounceIn",
+},
+out: {
+effect: "bounceOut",
+},
+
+});
+
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+      });
+
+    // Siri message animation
+    $('.siri-message').textillate({
         loop: true,
         sync: true,
         in: {
-            effect: "bounceIn",
+            effect: "fadeInUp",
+            sync: true,
         },
         out: {
-            effect: "bounceOut",
+            effect: "fadeOutUp",
+            sync: true,
         },
 
     });
