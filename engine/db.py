5b71b69f17e8a2396b8c06e7f8f66ba679c3cd31
Sanjeev Santhosh <sanjeevsanthosh143@gmail.com>
2025-08-12 21:53:52 +0530

    day 10 completed

--- engine/db.py
+++ engine/db.py
@@ -3,16 +3,25 @@ import sqlite3
 con = sqlite3.connect("jarvis.db")
 cursor = con.cursor()
 
-#query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-#cursor.execute(query)
-
-#query = "INSERT INTO sys_command VALUES (null,'Visual Studio Code', 'C:\\Users\\SANJEEV SANTHOSH\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code.exe')"
-#cursor.execute(query)
-#con.commit()
-
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
 
-query = "INSERT INTO web_command VALUES (null,'chatgpt', 'https://chatgpt.com/')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO sys_command VALUES (null,'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+# cursor.execute(query)
+# con.commit()
+
+
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+
+# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
+# cursor.execute(query)
+# con.commit()
+
+
+# testing module
+app_name = "android studio"
+cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+results = cursor.fetchall()
+print(results[0][0])
