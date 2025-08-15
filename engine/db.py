318277d9efb02c2576c8959402d2f2780d997154
Sanjeev Santhosh <sanjeevsanthosh143@gmail.com>
2025-08-15 12:24:59 +0530

    day 12 completed

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 con = sqlite3.connect("jarvis.db")
@@ -21,7 +22,34 @@ cursor.execute(query)
 
 
 # testing module
-app_name = "android studio"
-cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
-results = cursor.fetchall()
-print(results[0][0])
+#app_name = "android studio"
+#cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+#results = cursor.fetchall()
+#print(results[0][0])
+
+# Create a table with the desired columns
+#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+#desired_columns_indices = [0, 20]
+
+# Read data from CSV and insert into SQLite table for the desired columns
+#with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+    # csvreader = csv.reader(csvfile)
+     #for row in csvreader:
+      #   selected_data = [row[i] for i in desired_columns_indices]
+     #    cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# Commit changes and close connection
+#con.commit()
+#con.close()
+
+#query = "INSERT INTO contacts VALUES (null,'shekar', '8075159790', 'null')"
+#cursor.execute(query)
+#con.commit()
+
+#to fetch moblie no from contacts table
+ #query = 'kunal'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
